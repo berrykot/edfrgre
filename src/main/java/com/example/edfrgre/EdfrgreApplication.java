80b1e46ba3efe1b03794d9f3a247b41adec595e3
Константин Черника <berrykot@gmail.com>
2022-01-16 11:38:55 +0300

    Init

---
+++ src/main/java/com/example/edfrgre/EdfrgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.edfrgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdfrgreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdfrgreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/edfrgre/EdfrgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edfrgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdfrgreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
